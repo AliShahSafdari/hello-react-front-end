3feead7633e18e16b49c227cbbf523bafe8c8641
AliShahSafdari <alishahsafdari905@gmail.com>
2023-09-21 16:39:33 +0430

    Add components

---
+++ src/components/Greeting.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect} from 'react';
+import { fetchGreeting } from "../reducers/greetingReducer";
+
+function Greetings() {
+  const dispatch = useDispatch();
+  const { greeting } = useSelector(state => state.greeting);
+
+  useEffect(() => {
+    dispatch(fetchGreeting())
+  }, []);
+
+  return (
+    <div>
+      <div>
+      <h1>Random Greeting:</h1>
+      <h2>Greeting form API: {greeting}</h2>
+      <p> Please Refresh to get new greeting</p>
+      </div>
+    </div>
+  )
+}
+
+export default Greetings;
